8adf3bb1be291196bb66c43adc8319f87baf6a57
Pat Cavit <github@patcavit.com>
2016-11-07 23:44:33 -0800

    Streamline tests

--- test/ie11.js
+++ test/ie11.js
@@ -6,24 +6,36 @@ var assert = require("assert"),
 
 describe("postcss-fixie", function() {
     describe(":ie11()", function() {
+        function test(css, expected) {
+            return plugin.process(css).then((out) => assert.equal(out.toString(), expected));
+        }
+
         it("should transform :ie11(...) into an ie11 hack (single selector)", () =>
-            plugin.process(
-                ":ie11(.fooga) { color: red; }"
-            )
-            .then((result) => assert.equal(
-                result.toString(),
+            test(
+                ":ie11(.fooga) { color: red; }",
                 "_:-ms-fullscreen,:root .fooga { color: red; }"
-            ))
+            )
         );
 
         it("should transform :ie11(...) into an ie11 hack (multiple selectors)", () =>
-            plugin.process(
-                ":ie11(.fooga .wooga) { color: red; }"
-            )
-            .then((result) => assert.equal(
-                result.toString(),
+            test(
+                ":ie11(.fooga .wooga) { color: red; }",
                 "_:-ms-fullscreen,:root .fooga .wooga { color: red; }"
-            ))
+            )
+        );
+
+        it("should transform :ie11 ... into an ie11 hack (single selector)", () =>
+            test(
+                ":ie11 .fooga { color: red; }",
+                "_:-ms-fullscreen,:root  .fooga { color: red; }"
+            )
+        );
+        
+        it("should transform :ie11 ... into an ie11 hack (multiple selectors)", () =>
+            test(
+                ":ie11 .fooga .booga { color: red; }",
+                "_:-ms-fullscreen,:root  .fooga .booga { color: red; }"
+            )
         );
     });
 });
